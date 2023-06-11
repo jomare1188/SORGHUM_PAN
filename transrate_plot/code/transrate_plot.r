42d9781bbf7e926e4157e22cec496580ce4a018b
jomare1188 <jomare1188@gmail.com>
2023-06-11 19:42:39 -0300

    adding transrate analysis

--- transrate_plot/code/transrate_plot.r
+++ transrate_plot/code/transrate_plot.r
@@ -1,5 +1,5 @@
 # Get working directory
-wd="/home/j/SORGHUM_PAN/busco_plots/data/"
+wd="/home/j/SORGHUM_PAN/transrate_plot/data/"
 # Set current directory as wd
 setwd(wd)
 # load libraries
@@ -12,22 +12,24 @@ library(jsonlite)
 library(ggrepel)
 # Get files
 list_files <- list.files()
-list_files <- read.table("busco_files.txt")
+list_files <- read.table("genotype_list")
 prefix_df <- data.frame(prefix = substring(list_files$V1, 1, regexpr("_", list_files$V1) - 1))
-json_files <- paste0(prefix_df$prefix,"_busco.json")
+csv <- paste0(prefix_df$prefix,"_tr.csv")
 
 # Create auxiliary variables that we will use later
-json_files <- {}
+csv_files <- {}
 data <- data.frame()
 result <-data.frame()
+csv_full <- data.frame()
 
 # Iterate over prefixes 
 for (i in prefix_df$prefix){
-  json_files <- {}
-  json_files <- paste0(i,"_busco.json")
-  json_raw <- as.data.frame(read_json(json_files)$results)
-  json_raw$genotype <- i
-  data <- json_raw %>% select(-one_line_summary, -domain, -n_markers)
+  csv_files <- {}
+  csv_files <- paste0(i,"_tr.csv")
+  csv_raw <- read.table(csv_files, sep = ",", header = T)
+  csv_raw$genotype <- i
+  data <- csv_raw %>% select(genotype ,p_contigs_with_CRBB, p_refs_with_CRBB)
+  csv_full <- rbind(csv_full, csv_raw)
   result <- rbind(result, data)
 }
 
@@ -35,23 +37,25 @@ for (i in prefix_df$prefix){
 is_outlier <- function(x) {
   return(x < quantile(x, 0.25) - 1.5 * IQR(x) | x > quantile(x, 0.75) + 1.5 * IQR(x))
 }
+
 # Pivot data: transform matrix to a 3 column table
 pivoted_data <- result %>% pivot_longer(-c(genotype), names_to = "Statistic", values_to = "percentage") %>% group_by(Statistic) %>% mutate(outlier = if_else(is_outlier(percentage), genotype, NA_character_))
+
 
 # Define colores using wesanderson movie colors
 colors = wesanderson::wes_palettes$Darjeeling1
 
 # make results directory
-dir.create("./../results/")
+dir.create("/home/j/SORGHUM_PAN/transrate_plot/results")
 
 # make boxplot
 ggplot(pivoted_data, aes( x = Statistic, y = percentage, fill = Statistic)) +
   geom_boxplot(outlier.color = "red", outlier.size = 2, lwd=0.8, colour = "black", )+
-  geom_text_repel(aes(label = outlier), max.overlaps =5) +
-  labs(title="BUSCO",x="Metric", y = "Percentage")+
-  scale_fill_manual(values = colors) +
+  geom_text_repel(aes(label = outlier)) +
+  labs(title="Transrate",x="Metric", y = "Percentage")+
+  scale_fill_manual(values = colors[c(1,2)]) +
   theme_bw() +
-  theme( text = element_text(size=20),
+  theme( text = element_text(size=20, colour = "black"),
          legend.key.size = unit(1, 'cm'), 
          panel.border = element_blank(),
          panel.grid.major = element_blank(),
@@ -60,7 +64,8 @@ ggplot(pivoted_data, aes( x = Statistic, y = percentage, fill = Statistic)) +
          axis.text.y=element_text(colour="black"),
          axis.line = element_line(colour = "black", size = 1.2))
 # Save boxplot
-ggsave("./../results/busco_plot.png", device = "png", dpi= 300, width = 22, height = 20, units = "cm")
+ggsave("/home/j/SORGHUM_PAN/transrate_plot/results/transrate_plot.png", device = "png", dpi= 300, width = 22, height = 20, units = "cm")
 
-
+# summary other statistics
+summary(csv_full)
 
