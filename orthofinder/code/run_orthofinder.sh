99eaeb332305cea097ec4c7832b10cd610136e0c
jomare1188 <jomare1188@gmail.com>
2023-06-12 14:40:41 -0300

    run it in LOVELACE

--- orthofinder/code/run_orthofinder.sh
+++ orthofinder/code/run_orthofinder.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#PBS -q par16
-#PBS -l nodes=1:ppn=16
+#PBS -q par128
+#PBS -l nodes=1:ppn=128
 #PBS -N orthofinder
 
 cd $PBS_O_WORKDIR
-#mkdir -p ./../data
+mkdir -p ./../data
 
 # copy and rename .pep of transcoder to a new folder with $GENOTYPE.pep name to run orthofinder
 for i in $(cat ./../data/genotype_list)
@@ -18,7 +18,7 @@ mkdir -p ./../results
 # Run orthofinder for many inflation values
 for i in 1 1.3 1.5 1.8 2 2.5 3 4 5 6 7 10 15 20 30 40 50 100
 do
-	orthofinder -I ${i} -t 48 -a 8 -f ./../data/ -o ./../results/results_${i}
+	orthofinder -I ${i} -t 128 -a 64 -f ./../data/ -o ./../results/results_${i}
 done
 
 
